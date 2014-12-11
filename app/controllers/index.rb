77dc8e9a3d71d39db799d357aa74ce3893aaf108
Nirag Amin <nirag.amin@gmail.com>
2014-12-11 10:48:55 -0600

    Adding create new contact and pass test

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,17 @@ get '/contacts' do
   erb :'contacts/index'
 end
 
+#create new contact form
+get '/contacts/new' do
+  @contact = Contact.new
+  erb :'contacts/new'
+end
 
+#creates new contact
+post '/contacts' do
+  @contact = Contact.new(params[:contact])
+  @contact.save!
+  redirect '/contacts'
+end
 
 
